6531c07573fe1389886c022d1682103616527d81
ivuch <ivan.matellan@hotmail.com>
2015-05-08 11:33:57 -0300

    Arreglo

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,10 +1,12 @@
 import com.testapp.Role
 import com.testapp.User
 import com.testapp.UserRole
+import proyectocumple.Empresa
 
 class BootStrap {
 
 	def init = { servletContext ->
+        Empresa accenture = new Empresa(Ename: 'Accenture').save()
         User admin = new User(username:'admin', password:'admin', apellido:'apellido', fechaCumple:'0000-00-00 00:00:00', empresa:'1',  enabled:true).save()
         User emisomoza = new User(username:'emisomoza', password:'123456', enabled:true).save()
         User imatellan = new User(username:'imatellan', password:'1234', enabled:true).save()
